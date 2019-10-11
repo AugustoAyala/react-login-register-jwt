c1ca5f65ca72c7c4fdea7f4a5c6cc34d4b84c531
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2019-10-11 17:35:30 -0300

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -35,32 +35,55 @@ export default class Register extends Component {
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Register</div>
+                            <div className="card-header">
+                                Register
+                            </div>
                             <div className="card-body">
                                 <form onSubmit={this.onSubmit}>                 
                                      <div className="form-group row">
-                                         <label className="col-md-4 col-form-label text-md-right">Name</label>
+                                         <label className="col-md-4 col-form-label text-md-right">
+                                             Name
+                                         </label>
                                          <div className="col-md-6">
-                                           <input type="text" name="name" placeholder="name" required onChange={e => this.change(e)}/>
+                                             <input type="text"
+                                                    name="name" 
+                                                    placeholder="name"
+                                                    required 
+                                                    onChange={e => this.change(e)}
+                                              />
                                          </div>
                                      </div>
                                      <div className="form-group row">
-                                         <label className="col-md-4 col-form-label text-md-right">Email</label>
+                                         <label className="col-md-4 col-form-label text-md-right">
+                                             Email
+                                         </label>
                                          <div className="col-md-6">
-                                           <input type="email" name="email" placeholder="Email" required onChange={e => this.change(e)}/>
+                                             <input type="email"
+                                                    name="email"
+                                                    placeholder="Email"
+                                                    required 
+                                                    onChange={e => this.change(e)}
+                                              />
                                          </div>
                                      </div>
                                      <div className="form-group row">
-                                         <label className="col-md-4 col-form-label text-md-right">Password</label>
+                                         <label className="col-md-4 col-form-label text-md-right">
+                                             Password
+                                         </label>
                                          <div className="col-md-6">
-                                           <input type="password" name="password" placeholder="Password" required onChange={e => this.change(e)}/>
+                                             <input type="password"
+                                                    name="password"
+                                                    placeholder="Password" 
+                                                    required 
+                                                    onChange={e => this.change(e)}
+                                              />
                                          </div>
                                      </div>
                                      <div className="form-group row mb-0">
                                          <div className="col-md-8 offset-md-4">
-                                           <button type="submit" className="btb btn-primary">
-                                             Register
-                                           </button>                            
+                                             <button type="submit" className="btb btn-primary">
+                                                 Register
+                                             </button>                            
                                          </div>
                                      </div>
                                 </form>

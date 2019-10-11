7d7d4a3cd8894c7abe1b5a765c58111a8ad5a83e
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2019-10-11 17:29:21 -0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,15 +10,21 @@ export default class Navigation extends Component {
                     <Link className="navbar-brand" to="/">
                     React
                     </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon">
+                        </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
                                 <Link className="nav-link" to="/login">
-                                Login
+                                   Login
                                 </Link>
                             </li>
                             <li className="nav-item">
